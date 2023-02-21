33bfa585a5389134156f30c125e235842a6634b0
tylurh25 <tylertpham25@gmail.com>
2023-02-20 23:14:57 -0800

    final html knit and changes

--- chart2.R
+++ chart2.R
@@ -6,7 +6,7 @@ dataset<- read.csv ("Data Carrard et al. 2022 MedTeach.csv", stringsAsFactors = 
 custom_colors <- c("#0000FF", "#FF69B4", "#3CB043")
 label_names <- c("Male", "Female", "Non-Binary")
   
-  ggplot(data = dataset) +
+ chart2 <- ggplot(data = dataset) +
   geom_point(mapping = aes(x=mbi_ea, y = mbi_cy, color = factor(sex), alpha=I(0.5))) +
     labs(title = "Academic Efficiency & Cynical MBI Score", x = "Acadmeic Effciency MBI Score", y = "Cynical MBI Score", color = "Gender") +
     scale_color_manual(labels= label_names, values = custom_colors)
--- chart3.R
+++ chart3.R
@@ -1,10 +1,10 @@
 library(dplyr)
 library(ggplot2)
-mental_health_data <- read.csv("~/Desktop/INFO201/final-project-proposal-group-aa-3/Data-Carrard-2022-MedTeach.csv", stringsAsFactors = FALSE)
+mental_health_data <- read.csv("Data Carrard et al. 2022 MedTeach.csv", stringsAsFactors = FALSE)
 
 age_stud <- mental_health_data %>% group_by(age) %>% summarize(study_hours = sum(stud_h))
 
-ggplot(age_stud) +
+chart3 <- ggplot(age_stud) +
 geom_line(mapping = aes(x = age , y = study_hours)) +
   labs(title = "Correlation Between Age and Study Hours", 
        x = "Age", 
