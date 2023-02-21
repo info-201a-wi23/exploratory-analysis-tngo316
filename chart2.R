e4ff66a53dd7759a8f3314d1bd39de72622ecc0a
jasminempham <jasmine42582@gmail.com>
2023-02-20 20:32:37 -0800

    chart 2 updated again

--- chart2.R
+++ chart2.R
@@ -1,15 +1,8 @@
 
-
-
-
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
 library(scales)
-library("RColorBrewer")
 
-dataset<- read.csv ("/Users/jasminetran/Desktop/INFO201/final\ project/exploratory-analysis-tngo316/Data\ Carrard\ et\ al.\ 2022\ MedTeach.csv", stringsAsFactors = FALSE)
+dataset<- read.csv ("Data Carrard et al. 2022 MedTeach.csv", stringsAsFactors = FALSE)
 custom_colors <- c("#0000FF", "#FF69B4", "#3CB043")
 label_names <- c("Male", "Female", "Non-Binary")
   
@@ -17,7 +10,7 @@ label_names <- c("Male", "Female", "Non-Binary")
   geom_point(mapping = aes(x=mbi_ea, y = mbi_cy, color = factor(sex), alpha=I(0.5))) +
     labs(title = "Academic Efficiency & Cynical MBI Score", x = "Acadmeic Effciency MBI Score", y = "Cynical MBI Score", color = "Gender") +
     scale_color_manual(labels= label_names, values = custom_colors)
-  
+   
   
   
 
