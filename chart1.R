353099502e688225e6884bb7ef907dc8fd34cb72
tngo316 <121902483+tngo316@users.noreply.github.com>
2023-02-20 18:59:27 -0800

    chart 1

--- chart1.R
+++ chart1.R
@@ -1,7 +1,7 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+library(dplyr)
+
+chart1 <- ggplot(data = Data_Carrard_et_al_2022_MedTeach) +
+  geom_col(aes(x= year , y = mbi_ex, fill = year)) +
+  labs(x = "School Year", y = "Total MBI Emotional Score")
 
