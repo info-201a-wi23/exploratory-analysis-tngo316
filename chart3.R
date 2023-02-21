b7b52f2827cefc2a31cc38d4685810cbe3e56b1f
senudi-ds <121895162+senudi-ds@users.noreply.github.com>
2023-02-21 00:46:32 -0500

    Add files via upload

---
+++ chart3.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(ggplot2)
+mental_health_data <- read.csv("~/Desktop/INFO201/final-project-proposal-group-aa-3/Data-Carrard-2022-MedTeach.csv", stringsAsFactors = FALSE)
+
+age_stud <- mental_health_data %>% group_by(age) %>% summarize(study_hours = sum(stud_h))
+
+ggplot(age_stud) +
+geom_line(mapping = aes(x = age , y = study_hours)) +
+  labs(title = "Age Related to Study Hours", 
+       x = "Age", 
+       y = "Study Hours") +
+geom_point(mapping = aes(x = age , y = study_hours, color = "red")) +
+  labs(title = "Age Related to Study Hours", 
+       x = "Age", 
+       y = "Study Hours")
+
